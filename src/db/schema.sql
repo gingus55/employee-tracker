faf9febeb18530b476f9bf1d4669d04f0cccbd7f
gingus55 <gingus55@hotmail.com>
2021-12-05 10:08:47 +0000

    seed complete

--- src/db/schema.sql
+++ src/db/schema.sql
@@ -4,13 +4,13 @@ CREATE DATABASE tracker_db;
 USE tracker_db;
 
 CREATE TABLE department (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR(30) NOT NULL,
     PRIMARY KEY (id)
 );
 
 CREATE TABLE role (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL(2) NOT NULL,
     department_id INT,
@@ -21,7 +21,7 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOT NULL,
+    id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT,
---
+++ src/db/seeds.sql
@@ -0,0 +1,25 @@
+USE DATABASE tracker_db;
+
+INSERT INTO department (dept_name)
+VALUES ("Human Resources"),
+    ("IT"),
+    ("Teaching");
+
+INSERT INTO role (title, salary, department_id)
+VALUES ("teacher", 30000, 3),
+("admin", 25000, 1),
+("office", 22000, 1),
+("support", 18000, 3),
+("technical", 28000, 2),
+("phones", 23000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUES ("Bob", "Smith", 2, 1),
+("Charlie", "Brown", 1, 2),
+("Geoff", "Gilly", 4, 1),
+("Billy", "Nomates", 3, 3),
+("Vivien", "Westley", 5, 1),
+("Bruce", "Wayne", 6, 2),
+("Peter", "Parker", 5, 2),
+("Flash", "Gordon", 1, 3);
+
